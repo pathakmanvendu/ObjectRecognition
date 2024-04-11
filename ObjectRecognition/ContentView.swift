bd3f923d529643ec21f3b636f8f95eeb731d45ae
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-04-11 11:59:20 +0530

    Initial Commit

---
+++ ObjectRecognition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObjectRecognition
+//
+//  Created by manvendu pathak  on 11/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObjectRecognition/ObjectRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectRecognitionApp.swift
+//  ObjectRecognition
+//
+//  Created by manvendu pathak  on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ObjectRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
